8ef52c171b2373c677a1700f718fc7350881a977
haroldosales <haroldognu@gmail.com>
2021-01-06 22:27:02 -0300

    configuraçao backend

---
+++ backend/src/test/java/com/haroldosales/cursoDev/DeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haroldosales.cursoDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
